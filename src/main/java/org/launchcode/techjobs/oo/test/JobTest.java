6b3442c3ffd61d08d2483a9bdb913f07a8efdd13
cortesharobinson <crd3cc04@gmail.com>
2022-04-27 13:37:25 -0500

    Testing Unit Tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,10 +3,9 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -20,6 +19,25 @@ public class JobTest {
         Job testJob2 = new Job();
         assertEquals(testJob1.getId(), testJob2.getId(), 1);
         assertNotEquals(testJob1.getId(), testJob2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){ //test to see if all 6 fields are in the Job object and that they have been give their own unique id
+       Job testJob3 = new Job("Product tester", new Employer("ACE"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob3.getName() instanceof String);
+        assertTrue(testJob3.getEmployer() instanceof Employer);
+        assertTrue(testJob3.getLocation() instanceof Location);
+        assertTrue(testJob3.getPositionType() instanceof PositionType);
+        assertTrue(testJob3.getCoreCompetency() instanceof CoreCompetency);
+
+        // considers equal if they have the same id value; considers not equal if they don't have the same id value
+        assertEquals("Product Tester", testJob3.getName());
+        assertEquals("ACME", testJob3.getEmployer());
+        assertEquals("Desert", testJob3.getLocation());
+        assertEquals("Quality control", testJob3.getPositionType());
+        assertEquals("Persistence", testJob3.getCoreCompetency());
+
+
     }
 
 }
